fbd35d83bdddc3e463f5b16ab17d0795526fddc2
haifei <zhanghaifei@goumin.com>
2014-03-25 17:35:00 +0800

    1增加HFAndroidSample工程， 2增加JAVA与HTML相互调用Activity

---
+++ HFAndroidSample/src/com/haifei/ui/JavascriptInterface.java
@@ -0,0 +1,65 @@
+package com.haifei.ui;
+
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+/**
+ * JAVA 与 html通过javascript 相互调用的例子
+ * 
+ * @author: Haifei
+ * @data：2014-3-25
+ * @blog:http://blog.csdn.net/hellohaifei
+ *
+ */
+public class JavascriptInterface extends Activity {
+    private WebView webView;
+
+    @SuppressLint("JavascriptInterface")
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.javascriptinterface);
+        //加载页面
+        webView = (WebView) findViewById(R.id.webView);
+        //允许JavaScript执行
+        webView.getSettings().setJavaScriptEnabled(true);
+        //找到Html文件，也可以用网络上的文件
+        webView.loadUrl("file:///android_asset/index.html");
+        // 添加一个对象, 让JS可以访问该对象的方法, 该对象中可以调用JS中的方法
+        webView.addJavascriptInterface(new Contact(), "contact");
+        webView.addJavascriptInterface(new MyContact(), "mycontact");
+    }
+
+    private class Contact {
+        //JavaScript调用此方法拨打电话
+        public void call(String phone) {
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone)));
+        }
+
+        //Html调用此方法传递数据
+        public void showcontacts() {
+            String json = "[{\"name\":\"zxx\", \"amount\":\"9999999\", \"phone\":\"18600012345\"}]"; 
+            // 调用JS中的方法
+            webView.loadUrl("javascript:show('" + json + "')");
+        }
+    }
+    
+    private  class MyContact extends Contact{
+        //JavaScript调用此方法拨打电话
+        public void mycall(String phone) {
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone)));
+        }
+
+        //Html调用此方法传递数据
+        public void showcontacts() {
+            String json = "[{\"name\":\"zxx\", \"amount\":\"9999999\", \"phone\":\"18600012345\"}]"; 
+            // 调用JS中的方法
+            webView.loadUrl("javascript:show('" + json + "')");
+        }
+    }
+}
---
+++ HFAndroidSample/src/com/haifei/ui/MainActivity.java
@@ -0,0 +1,60 @@
+package com.haifei.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	
+	List<Custom> mList;
+	{
+		mList = new ArrayList<MainActivity.Custom>();
+		mList.add(new Custom("JAVA与HTML相互调用", JavascriptInterface.class));
+	}
+
+	
+	@SuppressLint("JavascriptInterface")
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+	}
+
+	Intent intent = new Intent();
+	void initView(){
+		LinearLayout root = (LinearLayout) findViewById(R.id.main_linearlayout);
+		
+		for (Custom item :mList) {
+			TextView tv = new TextView(this);
+			tv.setText(item.title);
+			tv.setTag(item.activityClass);
+			tv.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Class<?> activityClass = (Class<?>) v.getTag();
+					intent.setClass(MainActivity.this,activityClass);
+					startActivity(intent);
+				}
+			});
+			
+			root.addView(tv,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 50));
+		}
+	}
+	
+	class Custom{
+		String title;
+		Class<?> activityClass;
+		public Custom(String title, Class<?> c) {
+			this.title = title;
+			this.activityClass = c;
+		}
+	}
+}
