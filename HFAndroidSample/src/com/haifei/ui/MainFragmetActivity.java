2a70b16a59b494a7fbd4d09487dfd90b7db90ff6
hellohaifei <hellohaifei@163.com>
2015-07-10 14:59:57 +0800

    增加YoutubeActivity, 使用DragViewHelper

--- HFAndroidSample/src/com/haifei/ui/MainFragmetActivity.java
+++ HFAndroidSample/src/com/haifei/ui/MainFragmetActivity.java
@@ -33,9 +33,10 @@ public class MainFragmetActivity extends FragmentActivity {
 		mList.add(new Custom("SlidingPaneLayout", SlidingPaneLayoutFrabment.class));
 		mList.add(new Custom("ProcessBar帧动画", ProcessBarFragment.class));
 		mList.add(new Custom("自定义组件使用attr属性", CustomViewActivity.class));
+		mList.add(new Custom("Drag YoutubeActivity", YoutubeActivity.class));
 		
-		mList.add(new Custom("JAVA与HTML相互调用", JavascriptInterface.class));
-		mList.add(new Custom("自定义对话框", DialogActivity.class));
+		//mList.add(new Custom("JAVA与HTML相互调用", JavascriptInterface.class));
+		//mList.add(new Custom("自定义对话框", DialogActivity.class));
 	}
 
 	FrameLayout mFrameLayout;
